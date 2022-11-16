806d0f453d261c124e60cd6e38189cd8c51e29fd
CaffeineSnorter <118321363+CaffeineSnorter@users.noreply.github.com>
2022-11-15 19:49:23 -0600

    Update encoder.h

--- encoder.h
+++ encoder.h
@@ -1,4 +1,16 @@
 /*
+   ============================================================================
+                           ¡¡D I S C L A I M E R!!
+   ============================================================================
+   Estas a punto de leer el codigo más perro puerco de tu vida
+   si por alguna razón lo llegas a leer sientete libre de
+   mentarme la madre o enviarme una critica constructiva a:
+   
+                            Caffeine.Snorter@gmail.com
+                            
+  ==============================================================================
+  ==============================================================================
+
    encoder.h - Libreria para operar un encoder rotativo tipo potenciometro.
    Creada por CaffeineSnorter
 */
